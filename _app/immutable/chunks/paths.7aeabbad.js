3103ada402314c900b6d720c33a40703223b6347
AlexDShyft <151895390+AlexDShyft@users.noreply.github.com>
2024-02-06 23:15:36 -0500

    cancel target offer

---
+++ _app/immutable/chunks/paths.7aeabbad.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_igt4pr)==null?void 0:s.base)??"/Documentation";var t;const e=((t=globalThis.__sveltekit_igt4pr)==null?void 0:t.assets)??a;export{e as a,a as b};
