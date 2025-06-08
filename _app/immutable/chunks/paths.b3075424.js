4722148bb8744ed9b4311b6088da94206b30d3a4
AlexDShyft <151895390+AlexDShyft@users.noreply.github.com>
2025-06-08 10:52:12 -0400

    update
    update

--- _app/immutable/chunks/paths.b3075424.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1jua5o9)==null?void 0:s.base)??"/Documentation";var a;const e=((a=globalThis.__sveltekit_1jua5o9)==null?void 0:a.assets)??t;export{e as a,t as b};
